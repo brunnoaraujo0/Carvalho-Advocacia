019ec3caea4b0b95f03f8a07a509efc842904ec5
brunnoaraujo0 <helio.barros.708@ufrn.edu.br>
2022-11-29 13:43:15 -0300

    terceiro dia

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let margin = 0;
+
 document.querySelector('.material-symbols-outlined').addEventListener('click', ()=>{ 
     document.querySelector('.menu-hamburguer').style.opacity = 0; 
     document.querySelector('.menu-hamburguer').style.display = 'flex';
@@ -17,4 +19,51 @@ document.querySelector('.menu-hamburguer .material-symbols-outlined').addEventLi
     }, 200);
 
 
-})
+});
+
+document.querySelector('.servicos-seta-left .material-symbols-outlined').addEventListener('click', ()=>{
+    
+    if(window.innerWidth < 1400){
+        margin = margin + 280;
+    if(margin > 0 ){ 
+        margin = 0;
+    }
+    
+    document.querySelector('.servicos-list').style.marginLeft = margin + 'px';
+    }else {
+        margin = margin + 430;
+        if(margin > 0 ){ 
+            margin = 0;
+        }
+        
+        document.querySelector('.servicos-list').style.marginLeft = margin + 'px';
+    }
+    
+    
+
+});
+document.querySelector('.servicos-seta-right .material-symbols-outlined').addEventListener('click', ()=>{
+
+    if(window.innerWidth < 1400){
+        margin = margin - 280;
+    if(margin < (-3870)){
+        margin = (-3870);
+    }
+    
+    if(window.innerWidth - 3870 > margin) {
+        margin = (window.innerWidth - 4100);
+    }
+    document.querySelector('.servicos-list').style.marginLeft = margin + 'px';
+    }else {
+        margin = margin - 430;
+        if(margin < (-3870)){
+            margin = (-3870);
+        }
+        
+        if(window.innerWidth - 3870 > margin) {
+            margin = (window.innerWidth - 4100);
+        }
+        document.querySelector('.servicos-list').style.marginLeft = margin + 'px';
+    }
+    
+});
