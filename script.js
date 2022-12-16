c771fa095dd66b7318d861132a52e3ea0c5a378a
brunnoaraujo0 <helio.barros.708@ufrn.edu.br>
2022-12-16 15:00:37 -0300

    terminando

--- script.js
+++ script.js
@@ -210,3 +210,38 @@ ScrollReveal().reveal('.inicio-textos a', {
         z:0
     }
 })
+ScrollReveal().reveal('.servicos-titulo', {
+    delay: 400,
+    rotate:{
+        x:100,
+        y:0,
+        z:0
+    }
+})
+ScrollReveal().reveal('.advogado-titulo h1', {
+    delay: 400,
+    rotate:{
+        x:0,
+        y:100,
+        z:0
+    }
+})
+
+const menuItens = document.querySelectorAll('.header-menu a[href^="#"]'); //pega todos a
+
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick); //coloca um evento de click para cada a
+})
+
+function scrollToIdOnClick(event) { //esse event e passado cada a cada vez que e clicado, addeventlistener
+    event.preventDefault();
+    const element = event.target; // o event vem com varios atributos, o target pega so a tag "a"
+    const id = element.getAttribute('href'); //pega so atributo href dentro do element
+    const to = document.querySelector(id).offsetTop; //esse offsettop pega o top da section
+
+    window.scroll({
+        top: to - 100,
+        behavior: "smooth",
+    })
+}
+
